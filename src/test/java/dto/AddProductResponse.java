d70f5256de5ab34f1ee38a338a6de4a02eb99d19
Tetiana_Adamus <Tetiana_Adamus@epam.com>
2023-04-18 11:39:14 +0300

    first main commit

--- src/test/java/dto/AddProductResponse.java
+++ src/test/java/dto/AddProductResponse.java
@@ -5,21 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Class to parse response after adding the product
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddProductResponse {
-//    Product product;
+    Product product;
     Integer quantity;
-    String code;
+//    String code;
 
-//    @Data
-//    @JsonIgnoreProperties(ignoreUnknown = true)
-//    @AllArgsConstructor
-//    @NoArgsConstructor
-//    public class Product{
-//        String code;
-//
-//    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class Product{
+        String code;
+
+    }
 }
--- src/test/java/stepDefs/StepDefinitions.java
+++ src/test/java/stepDefs/StepDefinitions.java
@@ -47,67 +47,12 @@ public class StepDefinitions {
 
         CartCreateResponse cartCreatedObject = response.as(CartCreateResponse.class);
         sessionStorage.storeObject(GUID, cartCreatedObject.getGuid());
-
-// Перетворення відповіді у стрінгу:
-//       JsonPath jsonPath = response.jsonPath();
-//        String guid = jsonPath.get(GUID);
-//        sessionStorage.storeObject(GUID, guid);
-
-//        @Given("User created a cart via api")
-//        public void userCreatedACartViaApi() {
-//        Specification.requestSpecific(BASE_URL) - це неправильний підхід
-//        CartCreateResponse cartCreatedObject = given()
-//                .when()
-//                .post(CREATE_CART_URL)
-//                .then()
-//                .statusCode(SC_CREATED)
-//                .log().all()
-//                .extract()
-//                .as(CartCreateResponse.class);
-//
-//        sessionStorage.storeObject("guid", cartCreatedObject.getGuid());
     }
-
-//    @And("User add the product {string} to cart and {int} quantity via api")
-//    public void userAddTheProductToCartAndQuantityViaApi(String productCode, int quantity) {
-//        Response response= given()
-//                .spec(Specification.requestSpecific(BASE_URL))
-//                .body(addProductToCartPayload(productCode, quantity))
-//                .when()
-//                .post("/api/v2/kvn/users/anonymous/carts/"+sessionStorage.getStoreObject(GUID, String.class)+"/entries")
-//                .then()
-//                .statusCode(SC_OK)
-//                .extract()
-//                .response();
-//        response.jsonPath().getString(productCode);
-
-//    sessionStorage.storeObject("productCode", addProductResponse.getProduct().getCode());
-//        sessionStorage.storeObject("productQuantity", addProductResponse.getQuantity());
-//    }
 
     @And("User add the product {string} to cart with {int} quantity via api")
     public void userAddTheProductToCartAndQuantityViaApi(String productCode, int quantity) {
-
         InputStream createBookingJsonSchema = getClass().getClassLoader()
                 .getResourceAsStream("addproductjsonsheme.json");
-
-//        AddProductResponse addProductResponse = given()
-//                .spec(Specification.requestSpecific(BASE_URL))
-//                .body(addProductToCartPayload(productCode, quantity))
-//                .when()
-//                .post("/api/v2/kvn/users/anonymous/carts/{guid}/entries", sessionStorage.getStoreObject(GUID, String.class))
-//                .then()
-//                .statusCode(SC_OK)
-//                .and()
-//                .assertThat()
-//                .body(JsonSchemaValidator.matchesJsonSchema(createBookingJsonSchema))
-//                .and()
-//                .log().all()
-//                .extract()
-//                .as(AddProductResponse.class);
-//
-//        sessionStorage.storeObject("productCode", addProductResponse.getCode());
-//        sessionStorage.storeObject("productQuantity", addProductResponse.getQuantity());
 
         Response response = given()
                 .spec(Specification.requestSpecific(BASE_URL))
@@ -124,51 +69,21 @@ public class StepDefinitions {
                 .extract().response();
 
         String getProductCodeFromJson = response.jsonPath().getString("entry.product.code");
-        int getProductQuantityFronJson = response.jsonPath().getInt("quantity");
-        // response.jsonPath() - дає можливість витягнути потрібні дані одразу з json файлу, що повертається.
-        // Для цього файл не треба десь спеціально окремо зберігати.
-        //Важливо правильно прописати шлях до поля, яке ми хочемо взяти (схоже на xPath - прописуємо, починаючи від
-        // кореневого поля - більше тут: https://testerslittlehelper.wordpress.com/2019/01/20/jsonpath-in-rest-assured/
+        int getProductQuantityFromJson = response.jsonPath().getInt("quantity");
+
         sessionStorage.storeObject("productCode", getProductCodeFromJson);
-        sessionStorage.storeObject("productQuantity", getProductQuantityFronJson);
+        sessionStorage.storeObject("productQuantity", getProductQuantityFromJson);
+
+// Attempt to take a code value from POJO class:
+//        AddProductResponse addProductResponse = response.as(AddProductResponse.class);
+//        sessionStorage.storeObject("productCode", addProductResponse.getProduct().getCode());
     }
-
-//Створення пейлоаду через об'яєкти:
-//    private CartRequest addProductToCartPayload(String productCode, int qty) {
-//        CartRequest cartDTO = new CartRequest();
-//        ProductRequest productDTO = new ProductRequest();
-//        productDTO.setCode(productCode);
-//        cartDTO.setProduct(productDTO);
-//        cartDTO.setQuantity(qty);
-//        return cartDTO;
-//    }
-
-//    Створення пейлоаду через білдер:
-    private CartRequest addProductToCartPayload(String productCode, int qty) {
-            ProductRequest productRequest = ProductRequest.builder()
-                .code(productCode)
-                .build();
-        return CartRequest
-                .builder()
-                .product(productRequest)
-                .quantity(qty)
-                .build();
-    }
-
-    // через білдер і створення об'єкту:
-//        return CartRequest.builder()
-//                .product(new ProductRequest(productCode))
-//                .quantity(qty)
-//                .build();
-//    }
 
     @Then("{int} product {string} is added to cart via api")
     public void cartContainsProduct(int quantity, String product) {
         SoftAssertions softly = new SoftAssertions();
         softly.assertThat(sessionStorage.getStoreObject("productQuantity", Integer.class)).isEqualTo(quantity);
         softly.assertThat(sessionStorage.getStoreObject("productCode", String.class)).isEqualTo(product);
-//        assertThat(sessionStorage.getStoreObject("productQuantity", Integer.class)).isEqualTo(quantity);
-//        assertThat(sessionStorage.getStoreObject("productCode", String.class)).isEqualTo(product);
     }
 
     @When("User opens Home Page")
@@ -184,7 +99,6 @@ public class StepDefinitions {
         driver().getWebDriver().manage().addCookie(new Cookie(cookie, sessionStorage.getStoreObject(GUID, String.class)));
         waitForPageLoadComplete();
         driver().getWebDriver().navigate().refresh();
-        driver().getWebDriver().manage().getCookieNamed(cookie);
         waitForPageLoadComplete();
     }
 
@@ -202,7 +116,17 @@ public class StepDefinitions {
         waitForPageLoadComplete();
         assertThat(driver().getWebDriver().getCurrentUrl().contains(sessionStorage.getStoreObject("productCode",
                 String.class))).isTrue();
+    }
 
+    private CartRequest addProductToCartPayload(String productCode, int qty) {
+        ProductRequest productRequest = ProductRequest.builder()
+                .code(productCode)
+                .build();
+        return CartRequest
+                .builder()
+                .product(productRequest)
+                .quantity(qty)
+                .build();
     }
 
     private int getProductsNumberIntoBasket(){
